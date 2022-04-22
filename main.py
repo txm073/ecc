91244e1f8366585200e03c02896d6dd70ad1a20b
Tom <tomtombarnes2@gmail.com>
2022-04-22 21:58:34 +0100

    Files updated at 22/04/2022 - 21:58

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
+"""Elliptic Curve Diffie-Hellman Encryption algorithm implemented in Python"""
+
 import random
 
 
@@ -8,7 +9,7 @@ class EllipticCurve:
         "name": "secp256k1",
         # `p` is a large prime number used as a modulo
         # To cause the points on the curve to cycle back around to prevent huge numbers
-        "p": 2 ** 256 - 2 ** 32 - 2 ** 9 - 2 ** 8 - 2 ** 7 - 2 ** 6 - 2 ** 4 - 1, 
+        "p": 0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f, 
         # `a` and `b` are the constants in the elliptic curve general formula: `y²=x³+ax+b`
         "a": 0, 
         "b": 7, 
@@ -31,74 +32,89 @@ class EllipticCurve:
             return p
         # Check for vertical line
         if p[0] == q[0] and p[1] != q[1]:
-            return (np.inf, np.inf)
+            return (None, None)
         # If the points are the same use implicit differentiation
-        if p == q:
-            m = ((3 * p[0] ** 2 + self.params["a"]) * self.modinv(2 * p[1], self.params["p"])) % self.params["p"]        
-            x = (m ** 2 - 2 * p[0]) % self.params["p"]
-            y = (m * (p[0] - x) - p[1]) % self.params["p"]
+        if p[0] == q[0]:
+            m = (3 * p[0] * p[0] + self.params["a"]) * self.modinv(2 * p[1], self.params["p"])        
         # Calculate the line between two points (rise / run)
         else:        
-            m = (q[1] - p[1]) * self.modinv(q[0] - p[0], self.params["p"]) % self.params["p"]
-            x = (m ** 2 - p[0] - q[0]) % self.params["p"]
-            y = (m * (p[0] - x) - p[1]) % self.params["p"]
-        return x, y 
+            m = (p[1] - q[1]) * self.modinv(p[0] - q[0], self.params["p"]) 
+        x = m ** 2 - p[0] - q[0]
+        y = p[1] + m * (x - p[0])
+        return x % self.params["p"], -y % self.params["p"] 
 
     def mult(self, p, n):
         """Multiply point `p` by `n` using double and add algorithm"""
         if n == 0:
-            return (np.inf, np.inf)
+            return (None, None)
         # Result of addition
         result = None
-        # List to store the result of doubling previous result
-        stored = [p]
-        # Binomial expansion of n
-        powers = [i for i, bit in enumerate(bin(n)[2:][::-1]) if bit == "1"]
-        for i in range(1, powers[-1] + 1):
-            stored.append(self.add(stored[-1], stored[-1]))
-        # Add powers of 2 that make up n
-        for power in powers:
-            result = self.add(result, stored[power])
+        point = p
+        while n:
+            if n & 1:
+                # Add on the last iteratiion
+                result = self.add(result, point)
+            # Double
+            point = self.add(point, point)
+            # Bitwise right shift of n by 1 bit
+            n >>= 1
         return result
 
     def modinv(self, a, m):
         """Calculate the multiplicative inverse of `a` under modulo `m`"""
-        mod = m
-        x, y = 1, 0
-        if (m == 1):
-            return 0
-        while (a > 1):
-            q = a // m 
-            t = m
-            m = a % m
-            a = t
-            t = y
-            y = x - q * y
-            x = t
-        return x % mod
+        s, old_s = 0, 1
+        t, old_t = 1, 0
+        r, old_r = m, a
+        while r != 0:
+            quotient = old_r // r
+            old_r, r = r, old_r - quotient * r
+            old_s, s = s, old_s - quotient * s
+            old_t, t = t, old_t - quotient * t
+        gcd, x, y = old_r, old_s, old_t
+        return x % m
 
     def derive_keys(self):
+        """Derive a private/public keypair"""
         private = random.randrange(1, self.params["n"])
         public = self.mult((self.params["x"], self.params["y"]), private)
         return private, public
 
-# Curve: y^2 = x^3 - 7x + 10
-curve = EllipticCurve()
-a0, a1 = curve.derive_keys()
-b0, b1 = curve.derive_keys()
-print(a0)
+    def hcf(self, i, j):
+        """Find the highest common factor (gcd) of two integers"""
+        rem = i % j
+        while rem:
+            i = j
+            j = rem
+            rem = i % j
+        return j
 
-exit()
-alice_private_key, alice_public_key = curve.derive_keys()
-print("Alice's private key:", hex(alice_private_key))
-print("Alice's public key: (0x{:x}, 0x{:x})".format(*alice_public_key))
 
-# Bob generates his own key pair.
-bob_private_key, bob_public_key = curve.derive_keys()
-print("Bob's private key:", hex(bob_private_key))
-print("Bob's public key: (0x{:x}, 0x{:x})".format(*bob_public_key))
-# Alice and Bob exchange their public keys and calculate the shared secret.
-s1 = curve.mult(bob_public_key, alice_private_key)
-s2 = curve.mult(alice_public_key, bob_private_key)
-print(s1)
-print(s2)
+def main():
+    import hashlib
+
+    """
+    An overview of the algorithm:
+
+    - Both the client and server create a public/private key pair
+      using the same elliptic curve and the same parameters: (p, a, b, G, n, h)
+    - They then exchange their public keys (an x-y co-ordinate) and multiply the public key recieved
+      together with their own private key (a random integer between 1 and 2^256)
+    - This allows both the client and the server to establish a shared large number, which they can 
+      combine with a hashing function (i.e. SHA-256) to create a fixed-length shared key
+    - This shared key can be used to send information through a faster symmetric encryption 
+      method, such as AES (This is how it is used in the TLS/SSL handshake)
+    - The advantage to using Elliptic Curve Diffie-Hellman over traditional DH or RSA is that ECDHE
+      is less computationally intensive, so you can obtain the same level of security as RSA with much
+      shorter keys sizes, making encryption much faster
+    """
+    ec = EllipticCurve()
+    a0, a1 = ec.derive_keys()
+    b0, b1 = ec.derive_keys()
+    s1 = ec.mult(a1, b0)
+    s2 = ec.mult(b1, a0)
+    assert s1 == s2
+    secret = hashlib.sha256(str(s1[0]).encode()).hexdigest()
+    print("Shared secret:", secret)
+
+if __name__ == "__main__":
+    main()
